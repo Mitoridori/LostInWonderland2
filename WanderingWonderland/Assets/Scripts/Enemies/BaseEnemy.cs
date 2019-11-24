05b88912444a54ee4b0e76620e090de514017d03
John Gotts <44810602+Mitoridori@users.noreply.github.com>
2019-11-24 13:35:47 -0500

    Update BaseEnemy.cs
    work on enemy states, to be further worked on for extending to children

--- WanderingWonderland/Assets/Scripts/Enemies/BaseEnemy.cs
+++ WanderingWonderland/Assets/Scripts/Enemies/BaseEnemy.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseEnemy : MonoBehaviour
+public abstract class BaseEnemy : MonoBehaviour
 {
 
-    public int attackSpeed;
+    public float attackSpeed = 3f;
+    public GameObject weapon;
     public int attackRange;
 
     CharController player;
     Health health;
+    float nextAttack;
 
 
     private enum EnemyStates
@@ -22,16 +24,87 @@ public class BaseEnemy : MonoBehaviour
 
     private EnemyStates state = EnemyStates.waiting;
 
+    public abstract void Attack();
+
     // Start is called before the first frame update
     void Start()
     {
         player = FindObjectOfType<CharController>();
         health = GetComponent<Health>();
+        nextAttack = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateEnemy();
+    }
+
+    void UpdateEnemy()
+    {
+        if (state != EnemyStates.dead)
+        {
+            switch (state)
+            {
+                case EnemyStates.waiting:
+                    CheckIfActive();
+                    break;
+                case EnemyStates.attacking:
+                    StopAttacking();
+                    break;
+                case EnemyStates.roaming:
+                    FindTarget(attackRange);
+                    Movement();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+
+    void Movement()
+    {
+
+    }
+
+    void CheckIfActive()
+    {
+        if(gameObject.activeInHierarchy && state != EnemyStates.roaming)
+        {
+            state = EnemyStates.roaming;
+        }
+    }
+
+    void CheckIfCanAttack()
+    {
+        if(state == EnemyStates.attacking)
+        {
+            if(Time.time > nextAttack)
+            {
+                nextAttack = (Time.time + attackSpeed);
+                Attack();
+            }
+        }
+    }
+
+    void StopAttacking()
+    {
+        if (Vector2.Distance(transform.position, player.transform.position) > attackRange)
+        {
+            state = EnemyStates.roaming;
+        }
+    }
+
+    void FindTarget(float range)
+    {
+        if (player)
+        {
+            if (Vector2.Distance(transform.position, player.transform.position) <= range)
+            {
+                state = EnemyStates.attacking;
+                CheckIfCanAttack();
+            }
+        }
     }
 }
