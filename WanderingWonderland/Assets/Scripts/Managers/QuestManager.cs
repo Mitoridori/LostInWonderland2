37f4905b93127575c3dd0c85b6561329846f4c4f
Rebecca Stewart <44781452+beckrpgirl@users.noreply.github.com>
2019-11-25 16:46:06 -0500

    Added in NPC and quest
    Adding in NPC for the quest giver, along with creating prefabs for Allison, and the QuestGiver.

--- WanderingWonderland/Assets/Scripts/Managers/QuestManager.cs
+++ WanderingWonderland/Assets/Scripts/Managers/QuestManager.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class QuestManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    public TextMeshProUGUI TextDetails;
+   
+
     void Start()
     {
-        
+        TextDetails.text = "Quest log is currently empty.";
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        TextDetails.text = "s" ; //something grabbing the current text info
     }
 }
