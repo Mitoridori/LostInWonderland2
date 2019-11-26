4ea2b5aa70101b7c08b112cecc03f37dadad1f54
Rebecca Stewart <44781452+beckrpgirl@users.noreply.github.com>
2019-11-26 12:57:11 -0500

    Quest Log
    Quest NPC now holds a log for all of the quests,

--- WanderingWonderland/Assets/Scripts/Quests/ListofQuests.cs
+++ WanderingWonderland/Assets/Scripts/Quests/ListofQuests.cs
@@ -4,15 +4,71 @@ using UnityEngine;
 
 public class ListofQuests : MonoBehaviour
 {
+
+    public List<GameObject> QuestList;
+    int index;
+    bool gameIsWon = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (QuestList.Count != 0)
+        {
+            QuestList[0].SetActive(true);
+            index = 0;
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (QuestList.Count != 0)
+        {
+            SwitchPhase();
+        }
+
+    }
+
+    void SwitchPhase()
+    {
+        if (gameIsWon == false)
+        {
+            switch (QuestList[index].activeInHierarchy)
+            {
+                case true:
+                    break;
+
+                case false:
+                    IncremementIndex();
+                    QuestList[index].SetActive(true);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        else
+        {
+            GameOver();
+        }
+    }
+    void IncremementIndex()
+    {
+        if (index < QuestList.Count - 1)
+        {
+            index++;
+        }
+        else if (index == QuestList.Count - 1)
+        {
+            gameIsWon = true;
+            return;
+        }
+    }
+    //All quests are finished
+    void GameOver()
+    {
+        if (gameIsWon)
+        {
+            //what to do when game is over.
+        }
     }
 }
