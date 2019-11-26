33bb01fb85b5ee9038b7466cb86939958450d4f1
Greg McCready <52928801+reluctantlyd3ad@users.noreply.github.com>
2019-11-26 15:30:46 -0500

    Wood and coins
    Added wood script, added coins to GUI

--- WanderingWonderland/Assets/Scripts/Character/Character/Alison.cs
+++ WanderingWonderland/Assets/Scripts/Character/Character/Alison.cs
@@ -8,9 +8,7 @@ public class Alison : MonoBehaviour
     public int baseDamage = 10;
     private int PlayerCoins;
     public int PlayerStartCoins = 100;
-    private int PlayerWood;
-    public int PlayerStartWood = 10;
-
+    
     public CharController CC;
     private CharacterHealth healthBar;
     private int currentHealth;
@@ -67,15 +65,6 @@ public class Alison : MonoBehaviour
         return PlayerCoins;
     }
 
-    public int SetWood(int amount)
-    {
-        amount = PlayerWood;
-        return PlayerWood;
-    }
 
-    public int GetWood()
-    {
-        return PlayerWood;
-    }
 
 }
--- WanderingWonderland/Assets/Scripts/Collectibles/Wood.cs
+++ WanderingWonderland/Assets/Scripts/Collectibles/Wood.cs
@@ -8,27 +8,5 @@ public class Wood : BaseCollectible
     {
 
     }
-    private void Awake()
-    {
-        alison = FindObjectOfType<Alison>();
-
-    }
-    void PickupWood(int amount)
-    {
-        int i = alison.GetWood() + amount;
-        alison.SetWood(i);
-    }
-    void UseWood(int amount)
-    {
-
-        if (alison.GetWood() < amount)
-        {
-            //Doesn't have enough coins
-        }
-        else
-        {
-            int i = alison.GetWood() - amount;
-            alison.SetWood(i);
-        }
-    }
+   
 }
