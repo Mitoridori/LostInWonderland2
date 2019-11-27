7776aff8f73205787b2b61cc909b6cc3724b5a26
Rebecca Stewart <44781452+beckrpgirl@users.noreply.github.com>
2019-11-27 13:56:07 -0500

    Mass quest system
    working on a quest sytem overhall, make all quest making better when done.

--- WanderingWonderland/Assets/Scripts/Managers/QuestManager.cs
+++ WanderingWonderland/Assets/Scripts/Managers/QuestManager.cs
@@ -9,13 +9,19 @@ public class QuestManager : MonoBehaviour
     // Start is called before the first frame update
     public TextMeshProUGUI TextDetails;
     public TextMeshProUGUI TextTally;
+    public TextMeshProUGUI NPCBoxOne;
+    public TextMeshProUGUI NPCBoxTwo;
+
+    ListofQuests QuestNPC;
 
 
     void Start()
     {
-        NoQuestFound();
-
-
+        
+       NoQuestFound();
+       QuestNPC = FindObjectOfType<ListofQuests>();
+        if (QuestNPC)
+            Debug.Log("Quest NPC found");
     }
 
     // Update is called once per frame
@@ -26,12 +32,22 @@ public class QuestManager : MonoBehaviour
 
     private void FixedUpdate()
     {
-       
+        if (QuestNPC.QuestList.Count != 0)
+        {
+            //pass through quest info
+        }
+        else
+            NoQuestFound();
+
+
+
     }
 
     void NoQuestFound()
     {
         TextDetails.text = "Quest log is currently empty.";
         TextTally.text = " ";
+        NPCBoxOne.text = "Welcome Alison";
+        NPCBoxTwo.text = "Thank you for your help.";
     }
 }
---
+++ WanderingWonderland/Assets/Scripts/Quests/Tasks/Slayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slayer : Quest
+{
+
+    private void Start()
+    {
+        QuestName = "Ultimate Slayer";
+        Description = "Kill a bunch of stuff";
+        //ItemReward = ItemDataBase.Instance.GetItem("potion_log");
+        ExperenceRewart = 100;
+
+        Goals.Add(new BanditQuest(this, 0, "Kill 5 Slimes", false, 0, 5));
+        Goals.Add(new BanditQuest(this, 1, "Kill 2 vampires", false, 0, 2));
+
+        Goals.ForEach(g => g.Init());
+
+    }
+}
