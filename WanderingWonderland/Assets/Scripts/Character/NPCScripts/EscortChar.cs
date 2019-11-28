676f23aa3432d1a640b57e6be55e2417d580b00d
Jon Hunt <44199365+huntj1283@users.noreply.github.com>
2019-11-28 16:53:39 -0500

    Escort NPC
    Worked on escort npc, added movement functionality and  tracking the player

---
+++ WanderingWonderland/Assets/Scripts/Character/NPCScripts/EscortChar.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EscortChar : NPCController
+{
+
+    private NavMeshAgent agent;
+   
+    private int movespeed;
+    public int speed;
+    public Rigidbody rb;
+    bool PlayerisNear = false;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Alison);
+        Vector3 eulerAngles = transform.rotation.eulerAngles;
+        eulerAngles.x = 0;
+        eulerAngles.z = 0;
+        transform.rotation = Quaternion.Euler(eulerAngles);
+
+        rb.AddRelativeForce(0, 0, movespeed);
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "Player" && !PlayerisNear)
+        {
+            speedup();
+            PlayerisNear = true;
+        }
+
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        movespeed = 0;
+        PlayerisNear = false;
+    }
+
+    public void speedup()
+    {
+        movespeed = speed;
+    }
+
+    public override void Interact()
+    {
+
+    }
+
+
+
+}
