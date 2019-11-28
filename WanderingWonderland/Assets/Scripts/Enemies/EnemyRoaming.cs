b2d00273592b43e548fd0002fdd1c63942ccf3c2
John Gotts <44810602+Mitoridori@users.noreply.github.com>
2019-11-27 20:28:46 -0500

    work on AI, added code to level loading, added to build settings
    further work on the enemy AI to make them follow a path
    
    added the code to the level loading manager for loading the scenes
    
    added the scenes being used to the build settings for the game, no builds yet

--- WanderingWonderland/Assets/Scripts/Enemies/EnemyRoaming.cs
+++ WanderingWonderland/Assets/Scripts/Enemies/EnemyRoaming.cs
@@ -5,18 +5,56 @@ using UnityEngine.AI;
 
 public class EnemyRoaming : MonoBehaviour
 {
-
-    public Transform target;
     private NavMeshAgent agent;
+
+    public Transform WaypointContainer;
+    public Transform[] waypoints;
+    private int currentWaypoint = 0;
+    public float distanceToCover = 1f;
+
+    private float[] distanceLeftToTravel;
+
+
 
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        GetWaypoints();
+        distanceLeftToTravel = new float[waypoints.Length];
     }
 
     public void RoamingPath()
     {
-        agent.SetDestination(target.position);
+        for (int i = 0; i < waypoints.Length; i++)
+        {
+            Transform nextWaypoint = GetCurrentWaypoint();
+            float distanceCovered = (nextWaypoint.position - transform.position).magnitude;
+
+            if (distanceLeftToTravel[i] - distanceToCover > distanceCovered ||
+                waypoints[i] != nextWaypoint)
+            {
+                waypoints[i] = nextWaypoint;
+                distanceLeftToTravel[i] = distanceCovered;
+            }
+            agent.SetDestination(nextWaypoint.position);
+        }
+    }
+
+    void GetWaypoints()
+    {
+        Transform[] potentialWaypoints = WaypointContainer.GetComponentsInChildren<Transform>();
+        waypoints = new Transform[(potentialWaypoints.Length - 1)];
+
+        for (int i = 1; i < potentialWaypoints.Length; i++)
+        {
+            waypoints[i - 1] = potentialWaypoints[i];
+        }
+    }
+
+
+    public Transform GetCurrentWaypoint()
+    {
+        return waypoints[currentWaypoint];
     }
 
 }
--- WanderingWonderland/Assets/Scripts/Managers/LevelLoadingManager.cs
+++ WanderingWonderland/Assets/Scripts/Managers/LevelLoadingManager.cs
@@ -8,22 +8,22 @@ public class LevelLoadingManager : MonoBehaviour
 
     public void LoadLevelOne()
     {
-
+        SceneManager.LoadScene("LevelOne");
     }
 
     public void LoadOptionsMenu()
     {
-
+        SceneManager.LoadScene("OptionsMenu");
     }
 
     public void LoadInstructionsMenu()
     {
-
+        SceneManager.LoadScene("InstructionsMenu");
     }
 
     public void LoadMainMenu()
     {
-
+        SceneManager.LoadScene("MainMenu");
     }
 
     public void LoadWinScreen()
