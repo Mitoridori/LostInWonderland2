01a2fab1fd5f2658cf81b862bb8e9dd88a01422c
Greg McCready <52928801+reluctantlyd3ad@users.noreply.github.com>
2019-11-28 19:47:16 -0500

    Pushing Inventory system that is broken
    Here ye go

---
+++ WanderingWonderland/Assets/Inventory/ItemSlot.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ItemSlot : MonoBehaviour
+{
+
+    [SerializeField] Image Image;
+    private Item _item;
+    public Item item
+    {
+        get { return _item; }
+        set
+        {
+            _item = value;
+            if (item = null)
+            {
+                Image.enabled = false;
+            }
+            else
+            {
+                Image.sprite = _item.Icon;
+                Image.enabled = true;
+            }
+        }
+    }
+
+    private void OnValidate()
+    {
+        if (Image == null)
+          Image = GetComponent<Image>();
+        
+    }
+}
