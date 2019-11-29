adae87fd0ba0a6b9b4081c56b325f55738200655
Rebecca Stewart <44781452+beckrpgirl@users.noreply.github.com>
2019-11-28 19:05:48 -0500

    Billybob fix
    making Billybob follow you!

--- WanderingWonderland/Assets/Scripts/Character/NPCScripts/EscortChar.cs
+++ WanderingWonderland/Assets/Scripts/Character/NPCScripts/EscortChar.cs
@@ -7,51 +7,28 @@ public class EscortChar : NPCController
 {
 
     private NavMeshAgent agent;
+    public int followRange = 5;
    
-    private int movespeed;
-    public int speed;
-    public Rigidbody rb;
-    bool PlayerisNear = false;
+
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
- 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.LookAt(Alison);
-        Vector3 eulerAngles = transform.rotation.eulerAngles;
-        eulerAngles.x = 0;
-        eulerAngles.z = 0;
-        transform.rotation = Quaternion.Euler(eulerAngles);
-
-        rb.AddRelativeForce(0, 0, movespeed);
 
     }
-
-    public void OnTriggerEnter(Collider other)
+    private void FixedUpdate()
     {
+        FollowPlayer();
+    }
 
-        if (other.tag == "Player" && !PlayerisNear)
+    void FollowPlayer()
+    {
+        if ((Alison.transform.position - transform.position).magnitude <= followRange)
         {
-            speedup();
-            PlayerisNear = true;
+           agent.SetDestination(Alison.transform.position);
         }
-
-    }
-    public void OnTriggerExit(Collider other)
-    {
-        movespeed = 0;
-        PlayerisNear = false;
     }
 
-    public void speedup()
-    {
-        movespeed = speed;
-    }
 
     public override void Interact()
     {
